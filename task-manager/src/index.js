ef1b9ce0197e93f911b565d990668eac7f6e1319
heuzin <matheusqs.arq@gmail.com>
2021-07-12 10:59:41 -0400

    resource reading endpoints

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 require('./db/mongoose')
 const User = require('./models/userModel')
+const Task = require('./models/taskModel')
 
 const app = express()
 const port = process.env.PORT || 3000
@@ -13,7 +14,46 @@ app.post('/users', (req, res) => {
   user
     .save()
     .then(() => {
+      res.status(201).send(user)
+    })
+    .catch((error) => {
+      res.status(400).send(error)
+    })
+})
+
+app.get('/users', (req, res) => {
+  User.find({})
+    .then((users) => {
+      res.send(users)
+    })
+    .catch((error) => {
+      res.status(500).send()
+    })
+})
+
+app.get('/users/:id', (req, res) => {
+  const _id = req.params.id
+
+  User.findById(_id)
+    .then((user) => {
+      if (!user) {
+        return res.status(404).send()
+      }
+
       res.send(user)
+    })
+    .catch((error) => {
+      res.status(500).send()
+    })
+})
+
+app.post('/task', (req, res) => {
+  const task = new Task(req.body)
+
+  task
+    .save()
+    .then(() => {
+      res.status(201).send(task)
     })
     .catch((error) => {
       res.status(400).send(error)
