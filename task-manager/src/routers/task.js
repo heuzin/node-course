2869928dd1de025cf6d6c7e2f85911b4bce5c9e1
heuzin <matheusqs.arq@gmail.com>
2021-07-17 10:53:18 -0400

    working with timestamps, filtering data, paginating data and sorting data

--- task-manager/src/routers/task.js
+++ task-manager/src/routers/task.js
@@ -1,9 +1,13 @@
 const express = require('express')
 const router = new express.Router()
+const auth = require('../mddleware/auth')
 const Task = require('../models/taskModel')
 
-router.post('/task', async (req, res) => {
-  const task = new Task(req.body)
+router.post('/task', auth, async (req, res) => {
+  const task = new Task({
+    ...req.body,
+    owner: req.user._id,
+  })
 
   try {
     await task.save()
@@ -13,20 +17,52 @@ router.post('/task', async (req, res) => {
   }
 })
 
-router.get('/task', async (req, res) => {
+// GET /tasks?completed=true
+// GET /tasks?limit=10&skip=10
+// GET /tasks?sortBy=createdAt:asc or desc
+router.get('/task', auth, async (req, res) => {
+  const match = {}
+  const sort = {}
+
+  if (req.query.completed) {
+    match.completed = req.query.completed === 'true'
+  }
+
+  if (req.query.sortBy) {
+    const parts = req.query.sortBy.split(':')
+    sort[parts[0]] = parts[1] === 'desc' ? -1 : 1
+  }
+
+  if (req.query.sortBy) {
+    const parts = req.query.sortBy.split(':')
+    sort[parts[0]] = parts[1] === 'true' ? -1 : 1
+  }
+
   try {
-    const task = await Task.find({})
-    res.send(task)
+    // const task = await Task.find({ owner: req.user._id })
+    // res.send(task)
+    await req.user
+      .populate({
+        path: 'tasks',
+        match,
+        options: {
+          limit: parseInt(req.query.limit),
+          skip: parseInt(req.query.skip),
+          sort,
+        },
+      })
+      .execPopulate()
+    res.send(req.user.tasks)
   } catch (error) {
     res.status(500).send()
   }
 })
 
-router.get('/task/:id', async (req, res) => {
+router.get('/task/:id', auth, async (req, res) => {
   const _id = req.params.id
 
   try {
-    const task = await Task.findById(_id)
+    const task = await Task.findOne({ _id, owner: req.user._id })
 
     if (!task) {
       return res.status(404).send()
@@ -38,7 +74,7 @@ router.get('/task/:id', async (req, res) => {
   }
 })
 
-router.patch('/task/:id', async (req, res) => {
+router.patch('/task/:id', auth, async (req, res) => {
   const updates = Object.keys(req.body)
   const allowedUpdates = ['description', 'completed']
   const isValidOperation = updates.every((update) =>
@@ -50,25 +86,26 @@ router.patch('/task/:id', async (req, res) => {
   }
 
   try {
-    const task = await Task.findById(req.params.id)
-
-    updates.forEach((update) => (task[update] = req.body[update]))
-
-    await task.save()
+    const task = await Task.findOne({ _id: req.params.id, owner: req.user._id })
 
     if (!task) {
       return res.status(404).send()
     }
 
+    updates.forEach((update) => (task[update] = req.body[update]))
+    await task.save()
     res.send(task)
   } catch (error) {
     res.status(400).send(error)
   }
 })
 
-router.delete('/task/:id', async (req, res) => {
+router.delete('/task/:id', auth, async (req, res) => {
   try {
-    const task = await Task.findByIdAndDelete(req.params.id)
+    const task = await Task.findOneAndDelete({
+      _id: req.params.id,
+      owner: req.user._id,
+    })
 
     if (!task) {
       res.status(404).send()
